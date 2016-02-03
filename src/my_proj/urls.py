8a7818970740997c1b5753ed0002b8fc50a3838c
deeptir96 <deeptir96@gmail.com>
2016-02-03 16:11:50 +0530

    gb nc link

--- src/my_proj/urls.py
+++ src/my_proj/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^about/$', views.AboutPage.as_view(), name='about'),
     url(r'^users/', include(profiles.urls, namespace='profiles')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^menug.html/', views.GBNC.as_view(), name='gbnc'),
     url(r'^', include(accounts.urls, namespace='accounts')),
 ]
 
--- src/my_proj/views.py
+++ src/my_proj/views.py
@@ -7,3 +7,6 @@ class HomePage(generic.TemplateView):
 
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
+
+class GBNC(generic.TemplateView):
+    template_name = "menug.html"
